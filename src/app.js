06f5288c2c6496d47e387b2830831544a83769a1
DebbyArends <debby_arends@live.nl>
2022-02-03 17:13:26 +0100

    eerste opzet

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hallo daar!');
+// Installeren axios -> npm install axios
+// Enpoint hebben we straks nodig : https://restcountries.com
+// asynchrone functie moeten we aanmaken met een variabele
+
+import axios from "axios";
+
+async function fetchCountriedata() {
+    try{
+        const result = await axios.get('https://restcountries.com/v2/all')
+        console.log(result.data)
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+fetchCountriedata()
+
